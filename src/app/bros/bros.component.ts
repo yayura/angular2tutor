697cd6e5afca59e36fa1007ab9e44d7811e0f811
yayura <yuriy.yakovlev@arcadia.spb.ru>
2017-11-17 15:57:50 +0300

    list added *ngFor css class

--- src/app/bros/bros.component.ts
+++ src/app/bros/bros.component.ts
@@ -1,17 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Bro } from '../bro';
+import { BROS } from '../mock-bros';
 @Component({
   selector: 'app-bros',
   templateUrl: './bros.component.html',
   styleUrls: ['./bros.component.css']
 })
 export class BrosComponent implements OnInit {
-  bro: Bro = {
-    id:1,
-    name: "Windstorm"
-  };
+ 
+  bros = BROS;
+
+  selectedBro: Bro;
+
   constructor() { }
-    ngOnInit() {
+
+  ngOnInit() {
   }
 
+  
+  onSelect(bro: Bro): void {
+    this.selectedBro = bro;
+  }
+   
 }
